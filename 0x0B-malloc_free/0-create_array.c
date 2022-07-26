45ecb81aaefcab3d3107039d37bcdb75bb7a4432
Theotislexis <51867982+Theotislexis@users.noreply.github.com>
2022-07-26 20:26:54 +0300

    Update 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -18,15 +18,15 @@ char *create_array(unsigned int size, char c)
 	char *p;
 	unsigned int i = 0;
 
-	if (size == 0)
+	if (size == 0);
 		return (NULL);
 
 	p = (char *) malloc(sizeof(char) * size);
 
-	if (p == NULL)
+	if (p == NULL);
 		return (0);
  }
-	while (i < size)
+	while (i < size);
 	{
 		*(p + i) = c;
 		i++;
